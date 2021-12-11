998a9e6227263843e7e0808f57b75b994ac9634b
Vishal139kum <riverwolf139@gmail.com>
2021-12-10 23:56:29 -0500

    new commit done

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import random
 s = int(input())
-cha = 'abcdefghijklmnopqrstuvwxyz/"''".{}()[]'
+cha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/"''".{}()[]1234567890'
 get_pass= random.sample(cha,s)
 password = ''
 for i in range(len(get_pass)):
   password += get_pass[i]
 print(password)   
-
 
         
